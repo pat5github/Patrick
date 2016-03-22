56faeee84e9a00663cd0cc9f59c1e6fc78f38eb4
Richard Patrick <PRichar@schools.nyc.gov>
2016-03-22 10:05:57 -0400

    first commit

---
+++ Haiti.R
@@ -0,0 +1,23 @@
+library(ggmap)
+library(ggplot2)
+
+setwd('C:/GIS')
+sep <- read.csv("Haiti_SEP_assets_csv.csv")
+
+
+Sub1 <- sep[grep("SEP.12", names(sep))]
+
+
+sep$newCol <- 100*rowSums(Sub1)/rowSums(sep[3:6])
+
+
+# create a new grouping variable
+Percent_SEP12_Assets <- ifelse(sep[,7] >= 50, "Over 50", "Under 50")
+
+# get the map
+map <- get_map('Haiti', zoom = 9, maptype = 'hybrid')
+
+# plot the map and use the grouping variable for the fill inside the aes
+ggmap(map) +
+  geom_point(data=sep, aes(x = Longitude, y = Latitude, color=Percent_SEP12_Assets), size=6, alpha=0.6) +
+  scale_color_manual(breaks=c("Over 50", "Under 50"), values=c("green","red"))
